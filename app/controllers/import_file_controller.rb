252f3d6b0e0310e616c246c302e3de8b64ca1abf
Simone de Abreu <siabreu@gmail.com>
2022-02-06 16:42:31 -0300

    Define controller para importar arquivo XLSX

--- app/controllers/import_file_controller.rb
+++ app/controllers/import_file_controller.rb
@@ -1,7 +1,26 @@
+require 'roo'
+
 class ImportFileController < ApplicationController
   def index
   end
 
   def import
+    file = params[:file]
+    xlsx = Roo::Excelx.new(file)
+    my_hash = {}
+    xlsx.sheet(0).each(
+      name: 'Nome',
+      email: 'E-mail',
+      group: 'Grupo',
+      group_name: 'Nome do Grupo',
+      cpf: 'CPF',
+      phone: 'Telefone',
+      birth_date: 'Data Nascto',
+      address: 'End.') do |hash|
+        next if hash[:name] == 'Nome'
+        Prospect.create(hash)
+      end
+
+      redirect_to prospects_url, notice: "Prospects was successfully imported."
   end
 end
