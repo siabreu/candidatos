7a61685a1b3693d469a5915626abb10dbd39777d
Simone de Abreu <siabreu@gmail.com>
2022-02-06 16:43:01 -0300

    Inclui validações no model Prospect

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,3 +1,14 @@
 class Prospect < ApplicationRecord
-  validates :cpf, presence: true, uniqueness: true
+  validates :cpf, uniqueness: true
+
+  before_validation :clear_cpf
+  before_validation :convert_date
+
+  def clear_cpf
+    cpf&.gsub!(/\D/, '')
+  end
+
+  def convert_date
+    birth_date = Date.parse(birth_date) if birth_date.instance_of?(String)
+  end
 end
